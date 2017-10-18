69e2732a092bbb8e1c056392b5325b53092ae730
Juan Carlos Correa <juan.correa.herrera@gmail.com>
2017-10-18 16:21:30 -0500

    Login facebook if exists

--- app/Http/Controllers/SocialAuthController.php
+++ app/Http/Controllers/SocialAuthController.php
@@ -17,6 +17,16 @@ class SocialAuthController extends Controller
     public function callback()
     {
         $user = Socialite::with('facebook')->user();
+
+        $existing = User::whereHas('socialProfiles', function ($query) use ($user) {
+            $query->where('social_id', $user->id);
+        })->first();
+
+        if($existing !== null) {
+            auth()->login($existing);
+            return redirect('/');
+        }
+
         session()->flash('facebookUser', $user);
 
         return view('users.facebook', [
